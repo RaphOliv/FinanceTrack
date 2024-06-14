7b4259427c81fef62fb8c163daddb0fb3241802c
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-06-14 23:07:01 +0100

    layers created

--- app/src/main/java/com/hacksprint/financetrack/fragment/InfoBottomSheet.kt
+++ app/src/main/java/com/hacksprint/financetrack/fragment/InfoBottomSheet.kt
@@ -1,4 +1,4 @@
-package com.hacksprint.financetrack
+package com.hacksprint.financetrack.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.hacksprint.financetrack.R
 
 class InfoBottomSheet(
     private val title: String,
