9ea9989f45837acb75f82264469031960c8f1743
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-06-16 00:21:42 +0100

    layers created

--- app/src/main/java/com/hacksprint/financetrack/presentation/HomeActivity.kt
+++ app/src/main/java/com/hacksprint/financetrack/presentation/HomeActivity.kt
@@ -1,4 +1,4 @@
-package com.hacksprint.financetrack
+package com.hacksprint.financetrack.presentation
 
 import android.content.Intent
 import android.os.Bundle
@@ -11,6 +11,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
+import com.hacksprint.financetrack.R
 import java.text.NumberFormat
 import java.util.Locale
 
