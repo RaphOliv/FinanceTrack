7abcfa737b98134a6e5ba7fb33046b77d37a865f
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-06-15 23:47:28 +0100

    layers created

--- app/src/main/java/com/hacksprint/financetrack/presentation/NavigationActivity.kt
+++ app/src/main/java/com/hacksprint/financetrack/presentation/NavigationActivity.kt
@@ -1,15 +1,17 @@
-package com.hacksprint.financetrack
+package com.hacksprint.financetrack.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.hacksprint.financetrack.R
+import com.hacksprint.financetrack.fragment.HomeFragment
+import com.hacksprint.financetrack.fragment.MainFragment
 import com.hacksprint.financetrack.databinding.ActivityNavigationBinding
-
 
 class NavigationActivity : AppCompatActivity() {
 
 
-    private lateinit var binding: ActivityNavigationBinding
+    private lateinit var binding:ActivityNavigationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
