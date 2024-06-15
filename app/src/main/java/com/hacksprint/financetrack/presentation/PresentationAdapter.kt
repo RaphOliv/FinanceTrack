54a4f3b37b4bce089278dbbc136c57605c79ce35
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-06-15 23:48:59 +0100

    layers created

--- app/src/main/java/com/hacksprint/financetrack/presentation/PresentationAdapter.kt
+++ app/src/main/java/com/hacksprint/financetrack/presentation/PresentationAdapter.kt
@@ -1,8 +1,10 @@
-package com.hacksprint.financetrack
+package com.hacksprint.financetrack.presentation
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.hacksprint.financetrack.fragment.FrameLayoutFragment
+import com.hacksprint.financetrack.R
 
 // adapter que infla os frames de apresentacao inicial
 class PresentationAdapter (fragmentActivity: FragmentActivity):FragmentStateAdapter(fragmentActivity) {
